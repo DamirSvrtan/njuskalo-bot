21302730eaedd5fee3720675acec7dcd1b039dc9
Damir Svrtan <damir.svrtan@gmail.com>
2017-02-05 20:04:40 +0100

    Dont require pry

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,6 +1,5 @@
 require 'net/http'
 require 'time'
-require 'pry'
 
 SEARCH_URL = 'http://iapi.njuskalo.hr/?ctl=browse_ads&sort=new&categoryId=10920&locationId=2619&locationId_level_0=1153&locationId_level_1=1250&locationId_level_2=2619&price%5Bmin%5D=150&price%5Bmax%5D=400&mainAreaFrom=40&mainAreaTo=&adsWithImages=1&flatTypeId=0&floorCountId=0&roomCountId=0&flatFloorIdFrom=0&flatFloorIdTo=0&gardenAreaFrom=&gardenAreaTo=&balconyAreaFrom=&balconyAreaTo=&teraceAreaFrom=&teraceAreaTo=&yearBuiltFrom=&yearBuiltTo=&yearLastRebuildFrom=&yearLastRebuildTo='.freeze
 RECIPIENTS = ['damir.svrtan@gmail.com'].freeze
